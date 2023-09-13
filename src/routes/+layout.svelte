8ca4d39e95bbba47a8b1fc054ca3b0d5d815a45c
NelsonDJCR <dev@nelsondjcr.com>
2023-09-13 14:23:30 -0500

    clear

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,4 @@
 	preparePageTransition();
 </script>
 
-<main class="text-slate-700 max-w-4xl m-auto">
-	<div class="flex-col items-center justify-center py-4 px-4">
-		<h1 class="text-4xl font-bold mt-4">Template SvelteKit Mazter</h1>
-		<slot />
-	</div>
-</main>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,3 @@
 <svelte:head>
-	<title>Template Mazter SvelteKit</title>
+	<title></title>
 </svelte:head>
-
-<div class="w-[100%] mt-5">
-	<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-		<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-			<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-				<tr>
-					<th scope="col" colspan="2" class="px-6 py-3"> Dependences</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> PWA </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> View Transitions </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> Tailwind </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"><a href="https://akveo.github.io/eva-icons" target="_blank">Icons</a> </td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-</div>
-
-
-
-<div class="d-flex justify-center items-center">
-	<a href="/home"><img src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg" style:--tag={'taylor'} alt="taylor" class="w-[80px]" /></a>
-	<br>
-	<h1 style:--tag={'title'} class="font-bold">Cristinao</h1>
-</div>
